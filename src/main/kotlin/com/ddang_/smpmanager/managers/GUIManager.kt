cbd8eab82fc44611a36129f3fa34ddad0801c0a8
DDang_ <psychologist3552@gmail.com>
2022-06-26 18:52:38 +0900

    1.0.1

--- src/main/kotlin/com/ddang_/smpmanager/managers/GUIManager.kt
+++ src/main/kotlin/com/ddang_/smpmanager/managers/GUIManager.kt
@@ -4,7 +4,9 @@ import com.ddang_.smpmanager.enums.Color
 import com.ddang_.smpmanager.enums.InventoryName
 import com.ddang_.smpmanager.guis.MenuGUI
 import com.ddang_.smpmanager.utils.ComponentUtil
+import com.ddang_.smpmanager.utils.ItemUtil
 import org.bukkit.Bukkit
+import org.bukkit.Material
 import org.bukkit.inventory.Inventory
 
 class GUIManager {
@@ -16,6 +18,47 @@ class GUIManager {
                         "메뉴", "000000"
                     ))
 
+                    var item = ItemUtil.toItem(
+                        Material.PLAYER_HEAD, 1,
+                        ComponentUtil.toText("모든 플레이어 재배치", Color.LIME.code),
+                        arrayListOf(
+                            ComponentUtil.toText("", Color.WHITE.code)
+                        )
+                    )
+                    ItemUtil.applyStringPDC(item, "identify", "0")
+                    i.setItem(0, item)
+
+                    item = ItemUtil.toItem(
+                        Material.CRAFTING_TABLE, 1,
+                        ComponentUtil.toText("특수 조합법 목록", Color.LIME.code),
+                        arrayListOf(
+                            ComponentUtil.toText("", Color.WHITE.code)
+                        )
+                    )
+                    ItemUtil.applyStringPDC(item, "identify", "1")
+
+                    i.setItem(1, item)
+
+                    item = ItemUtil.toItem(
+                        Material.SKELETON_SKULL, 1,
+                        ComponentUtil.toText("사망시 무작위 위치 재배치", Color.LIME.code),
+                        arrayListOf(
+                            ComponentUtil.toText("", Color.WHITE.code)
+                        )
+                    )
+                    ItemUtil.applyStringPDC(item, "identify", "2")
+                    i.setItem(2, item)
+
+                    item = ItemUtil.toItem(
+                        Material.COMPASS, 1,
+                        ComponentUtil.toText("특정 월드 좌표와 채팅 가리기", Color.LIME.code),
+                        arrayListOf(
+                            ComponentUtil.toText("", Color.WHITE.code)
+                        )
+                    )
+                    ItemUtil.applyStringPDC(item, "identify", "3")
+
+                    i.setItem(3, item)
                     return i
                 }
             }
